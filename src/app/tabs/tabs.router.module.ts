e9920e11334382f6f84fe2b54c9266f51b2cb756
Gustavo Peralta <guper08@gmail.com>
2019-06-05 08:01:49 +0200

    - New sencond project

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -6,47 +6,15 @@ const routes: Routes = [
   {
     path: 'tabs',
     component: TabsPage,
-    children: [
-      {
-        path: 'tab1',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab1/tab1.module#Tab1PageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab2',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab2/tab2.module#Tab2PageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab3/tab3.module#Tab3PageModule'
-          }
-        ]
-      },
-      {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
-    ]
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
-];
+    children: [ 
+      { path: 'tab1',  children: [{ path: '', loadChildren: '../tab1/tab1.module#Tab1PageModule' }] },
+      { path: 'tab2',  children: [{ path: '', loadChildren: '../tab2/tab2.module#Tab2PageModule' }] },
+      { path: 'tab3',  children: [{ path: '', loadChildren: '../tab3/tab3.module#Tab3PageModule' }] },
+      { path: 'formulario', children: [{ path: '', loadChildren: '../formulario/formulario.module#FormularioPageModule' }] },
+      { path: '', redirectTo: '/tabs/tab1', pathMatch: 'full' }
+    ] },
+    
+    { path: '', redirectTo: '/tabs/tab1', pathMatch: 'full' }];
 
 @NgModule({
   imports: [
