56b0349b3674b1501f3e1691f228f3a0860a67e3
Gustavo Peralta <guper08@gmail.com>
2019-06-06 07:32:23 +0200

    -

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  public seleccion:string = "grupo1";
+
   constructor() {}
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  public lista  = [
+    { titulo: 'Tit1', id: 1, descripcion: 'Lorem ipsum dolor'},
+    { titulo: 'Tit2', id: 2, descripcion: 'Lorem ipsum dolor'},
+    { titulo: 'Tit3', id: 3, descripcion: 'Lorem ipsum dolor'},
+    { titulo: 'Tit4', id: 4, descripcion: 'Lorem ipsum dolor'},
+    { titulo: 'Tit5', id: 5, descripcion: 'Lorem ipsum dolor'},
+    { titulo: 'Tit6', id: 6, descripcion: 'Lorem ipsum dolor'},
+  ]
+
   constructor() {}
 
 }
